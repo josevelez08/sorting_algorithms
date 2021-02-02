82a47872c273308c1789a537eaf743ec14390df9
josevelez08 <jose-0919@hotmail.com>
2021-02-02 07:32:59 +0000

    Task 0 bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,33 @@
 #include "sort.h"
 /**
+ *  swap - the code about bubble sort algorithm
+ * @xp: array
+ * @yp: size of array
+ */
+void swap(int *xp, int *yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+/**
  *  bubble_sort - the code about bubble sort algorithm
  * @array: array
  * @size: size of array
  */
 void bubble_sort(int *array, size_t size)
 {
-	int tmp;
-	size_t j = 0, i = 0;
+	size_t i, j;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
 			}
 		}
-		print_array(array, size);
 	}
 }

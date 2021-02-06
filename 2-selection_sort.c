fecd1ee4df5b94628a2b6449616d2191d8984eea
josevelez08 <jose-0919@hotmail.com>
2021-02-06 09:40:24 +0000

    Selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+/**
+ * swap -  change positions xp for yp
+ * @xp: minimun
+ * @yp: older
+ */
+void swap(int *xp, int *yp)
+{
+	int temp = *xp;
+
+	*xp = *yp;
+	*yp = temp;
+}
+/**
+ * selection_sort - algorithm selection_sort
+ * @array: array of characters to sort
+ * @size: size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t step = 0;
+	size_t j;
+
+	for (step = 0; step < size - 1; step++)
+	{
+		size_t min = step;
+
+		for (j = step + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+		swap(&array[min], &array[step]);
+		print_array(array, size);
+	}
+}
